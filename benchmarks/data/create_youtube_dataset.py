e89a15485926b70e83c1e7d8cec9f15a6fc71a11
gony.noreply <gony.noreply@kakaocorp.com>
2020-04-01 13:45:14 +0900

    add distance to youtube hdf5 database

--- benchmarks/data/create_youtube_dataset.py
+++ benchmarks/data/create_youtube_dataset.py
@@ -1,8 +1,12 @@
 import sys
+sys.path.append('..')
 
 import h5py
 import numpy
+
 from sklearn.model_selection import train_test_split
+
+from distance import metrics as pd
 
 
 class BruteForceBLAS():
@@ -19,14 +23,21 @@ class BruteForceBLAS():
             self.index = numpy.ascontiguousarray(X, dtype=self._precision)
             self.lengths = numpy.ascontiguousarray(lens, dtype=self._precision)
 
-    def query(self, v, n):
-        v = numpy.ascontiguousarray(v, dtype=self._precision)
+    def query_with_distances(self, v, n):
+        v = numpy.ascontiguousarray(v, dtype=self.index.dtype)
         if self._metric == 'angular':
             dists = -numpy.dot(self.index, v)
         elif self._metric == 'euclidean':
             dists = self.lengths - 2 * numpy.dot(self.index, v)
+        else:
+            assert False, "invalid metric"
         indices = numpy.argpartition(dists, n)[:n]
-        return sorted(indices, key=lambda index: dists[index])
+
+        def fix(index):
+            ep = self.index[index]
+            ev = v
+            return (index, pd[self._metric]['distance'](ep, ev))
+        return map(fix, indices)
 
 
 def write_output(train, test, fn, distance, point_type='float', count=100):
@@ -36,20 +47,22 @@ def write_output(train, test, fn, distance, point_type='float', count=100):
     f.create_dataset('train', (len(train), len(train[0])), dtype=train.dtype)[:] = train
     f.create_dataset('test', (len(test), len(test[0])), dtype=test.dtype)[:] = test
     neighbors = f.create_dataset('neighbors', (len(test), count), dtype='i')
+    distances = f.create_dataset('distances', (len(test), count), dtype='f')
     bf = BruteForceBLAS(distance, precision=train.dtype)
     bf.fit(train)
     for i, x in enumerate(test):
         sys.stderr.write('computing queries %d/%d ...\r' % (i+1, len(test)))
-        neighbors[i] = bf.query(x, count)
-
+        res = list(bf.query_with_distances(x, count))
+        res.sort(key=lambda t: t[-1])
+        neighbors[i] = [j for j, _ in res]
+        distances[i] = [d for _, d in res]
     sys.stderr.write('\n')
     f.close()
 
 
-def youtube(out_fn):
-    fn = './data/youtube.txt'
+def youtube(in_fn, out_fn):
     X = []
-    for line in open(fn):
+    for line in open(in_fn):
         v = [float(x) for x in line.strip().split()]
         X.append(numpy.array(v))
 
@@ -57,7 +70,6 @@ def youtube(out_fn):
     write_output(numpy.array(X_train), numpy.array(X_test), out_fn, 'angular')
 
 
-
 if __name__ == '__main__':
-    out_fn = os.path.join('data', 'youtube-40-angular.hdf5')
-    youtube(out_fn)
+    youtube('youtube1m.txt', 'youtube1m-40-angular.hdf5')
+    youtube('youtube.txt', 'youtube-40-angular.hdf5')
