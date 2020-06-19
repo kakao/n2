0602f2865d980d3830bee64e0f005f643e6300c8
gony.noreply <gony.noreply@kakaocorp.com>
2020-06-19 23:41:21 +0900

    change benchmark params

--- benchmarks/benchmark_script.py
+++ benchmarks/benchmark_script.py
@@ -223,10 +223,8 @@ def get_fn(file_type, args, base=CACHE_DIR):
 def run(args):
     results_fn = get_fn('result', args, base=RESULT_DIR) + '.txt'
 
-    # index_params = [(12, 100)]
-    # query_params = [25, 50, 100, 250, 500, 750, 1000, 1500, 2500, 5000, 10000]
-    index_params = [(32, 800)]
-    query_params = [25, 50, 100, 250, 500, 750, 1000, 1500, 2500]
+    index_params = [(24, 400)]
+    query_params = [25, 50, 100, 250, 500, 750, 1000, 1500, 2500, 5000]
 
     algos = {
         'n2': [N2(M, ef_con, args.n_threads, ef_search, args.distance)
